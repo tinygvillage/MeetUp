a5c70dac9a84ad0bb532c6f75bc6c9ec4a714d7a
tinygvillage <connect@refusionist.com>
2023-03-28 19:02:37 -0700

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
@@ -14,5 +16,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
