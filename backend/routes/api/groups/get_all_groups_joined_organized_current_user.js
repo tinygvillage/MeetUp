d9b9a511b899f6938ecd0a38717e6258f505a05c
tinygvillage <connect@refusionist.com>
2023-05-17 20:22:49 -0400

    re-organizing all validation and helper function modules

--- backend/routes/api/groups/get_all_groups_joined_organized_current_user.js
+++ backend/routes/api/groups/get_all_groups_joined_organized_current_user.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
+
 const { Group, Membership, GroupImage } = require('../../../db/models');
+const { requireAuth } = require('../../../utils/auth');
 const { formatGroup } = require('../../../utils/formatGroup');
 
-router.get('/', async (_req, res, next) => {
+router.get('/current', requireAuth, async (req, res, next) => {
+
+    const { user } = req;
 
     const groups = await Group.unscoped().findAll({
+        where: { organizerId: user.id },
         include: [{ model: Membership }, { model: GroupImage, attributes: ["url"] }]
     });
 
-    if (!groups) next(err);
+    if (!groups) next(err)
 
-    const groupInfo = formatGroup(groups);
+    const groupInfo = await formatGroup(groups);
 
     return res.status(200).json({ "Groups": groupInfo });
 });
-
 
 module.exports = router;
